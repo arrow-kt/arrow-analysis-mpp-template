76da29335847e0585e2ceaa6632d3bd5743bdcf4
Alejandro Serrano <trupill@gmail.com>
2022-01-31 15:28:16 +0100

    Update to Arrow Analysis 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,18 @@
 plugins {
   kotlin("multiplatform") version "1.6.10" apply true
-  id("io.kotest.multiplatform") version "5.0.3" apply true
+  id("io.kotest.multiplatform") version "5.1.0" apply true
 }
 
 group "org.example"
 version "1.0"
 
 repositories {
-  maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
   mavenCentral()
 }
 
 buildscript {
-  repositories {
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
-  }
   dependencies {
-    classpath("io.arrow-kt.analysis.kotlin:io.arrow-kt.analysis.kotlin.gradle.plugin:2.0-SNAPSHOT")
+    classpath("io.arrow-kt.analysis.kotlin:io.arrow-kt.analysis.kotlin.gradle.plugin:2.0")
   }
 }
 
@@ -40,9 +36,9 @@ kotlin {
     }
     commonTest {
       dependencies {
-        implementation("io.kotest:kotest-property:5.0.3")
-        implementation("io.kotest:kotest-framework-engine:5.0.3")
-        implementation("io.kotest:kotest-assertions-core:5.0.3")
+        implementation("io.kotest:kotest-property:5.1.0")
+        implementation("io.kotest:kotest-framework-engine:5.1.0")
+        implementation("io.kotest:kotest-assertions-core:5.1.0")
       }
     }
   }
